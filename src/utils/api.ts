7a0fe1867febc8384a2d73a8d6d8a11ae38b7a16
Sardorbek <sardorbekmusilman@gmail.com>
2026-01-16 16:35:41 +0500

    hh

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000";
+  import.meta.env.VITE_API_BASE_URL || "http://104.131.48.1:4000";
 
 export const api = axios;
